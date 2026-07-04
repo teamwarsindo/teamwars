0fe0ebf360456c57e6b8d76f2fac44b1cb42ced2
Team Wars Indonesia <teamwars.indo@gmail.com>
2026-07-04 20:54:12 +0700

    Remove description from HeroHeader
    Updated HeroHeader component to remove description.

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -88,7 +88,7 @@ export default function RulebookPage() {
       <TopBar title="Official Rulebook" />
 
       <div className="relative z-10 flex w-full flex-1 flex-col items-center px-4 pb-4 sm:px-6">
-        <HeroHeader description="Baca dan pahami seluruh regulasi sebelum bertanding. Ketidaktahuan akan peraturan tidak membebaskan peserta dari sanksi." />
+        <HeroHeader />
 
         <section className="flex w-full max-w-4xl flex-col items-center">
           
