28e9f901328c8f4dfed243a7b0d0d2aff2aca3c8
Team Wars Indonesia <teamwars.indo@gmail.com>
2026-07-18 15:35:54 +0700

    Add exports for buttons and commands modules

--- lib/discord/index.ts
+++ lib/discord/index.ts
@@ -1,4 +1,12 @@
+// ⚙️ Core & Utilities
+export * from './config';
+export * from './utils';
+
+// 🛠️ Features (File tunggal)
 export * from './roles';
 export * from './channels';
+
+// 📂 Modul Folder (Akan otomatis membaca index.ts di dalam foldernya)
 export * from './messages';
-// export * from './config'; (Opsional jika ingin di-export dari sini juga)
+export * from './buttons';
+export * from './commands';
