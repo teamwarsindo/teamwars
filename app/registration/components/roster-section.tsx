4d35d7fa110830e250869194673aab7ce3712756
Team Wars Indonesia <teamwars.indo@gmail.com>
2026-07-23 11:56:46 +0700

    Update import paths for validators and registration

--- app/registration/components/roster-section.tsx
+++ app/registration/components/roster-section.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import { TrashIcon, PlusIcon, AlertIcon, CheckIcon } from "@/components/icons"
-import { formatDuelId, sanitizeRealName, sanitizeDiscord, toProperCase } from "../utils/validators"
-import { ROSTER_ROLES, MIN_PLAYERS, MAX_PLAYERS, type Player, type RosterRole } from "../utils/lib-registration"
+import { formatDuelId, sanitizeRealName, sanitizeDiscord, toProperCase } from "@/lib/validators"
+import { ROSTER_ROLES, MIN_PLAYERS, MAX_PLAYERS, type Player, type RosterRole } from "@/lib/registration"
 
 interface RosterSectionProps {
   players: Player[]
