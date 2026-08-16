0028e183d3174812fc91363cf3eac014166cefc2
Team Wars Indonesia <teamwars.indo@gmail.com>
2026-08-16 22:00:11 +0700

    Add categoryI for Team Wars Indonesia Season 7

---
+++ app/rules/components/categories/category-i.ts
@@ -0,0 +1,20 @@
+import { RuleCategory } from "../rules-types";
+
+export const categoryI: RuleCategory = {
+  id: "I",
+  title: "Struktur Organisasi Team Wars Indonesia Season 7",
+  rules: [
+    {
+      title: "Jajaran Kepengurusan",
+      points: [
+        "a. Presiden: Pradinata",
+        "b. Administrator: Tsaqif",
+        "c. Head of Finance: Elthor",
+        "d. Head of Competition: Rexia",
+        "e. Chief Referee: Xenon",
+        "f. Lead Data Analyst: Fajar Haikal",
+        "g. Head Of Creative: Nazz"
+      ]
+    }
+  ]
+};
