ba8ef8bc908b62fce82a1589493d8eae72f854e7
Team Wars Indonesia <teamwars.indo@gmail.com>
2026-08-16 15:57:28 +0700

    Hapus Format Hardcore

--- app/_home-components/tournament-hub.tsx
+++ app/_home-components/tournament-hub.tsx
@@ -222,7 +222,6 @@ export function TournamentHub() {
           todayMatches={todayMatches}
           upcomingMatches={upcomingMatches}
           recentResults={recentResults}
-          formatDate={formatDate}
         />
 
         <StandingsSnapshot
