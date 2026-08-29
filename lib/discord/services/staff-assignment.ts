c22aae647f3274023150c59e24a87ce30b4dc1f8
Team Wars Indonesia <teamwars.indo@gmail.com>
2026-08-29 16:11:44 +0700

    Export executeSwapAssignStaff from staff-swap-service
    Added executeSwapAssignStaff function export to the module.

--- lib/discord/services/staff-assignment.ts
+++ lib/discord/services/staff-assignment.ts
@@ -3,6 +3,7 @@ import { DISCORD_CONFIG } from '@/lib/discord/config';
 export { type StaffItem } from './staff-helpers';
 export { executeAssignStaff } from './staff-assign-service';
 export { executeUnassignStaff } from './staff-unassign-service';
+export { executeSwapAssignStaff } from './staff-swap-service';
 
 export function isDiscordAuthorized(interaction: any): boolean {
   const member = interaction?.member;
