428e13ae1af09f954b97dbf25db56b27a5f9ecdc
Team Wars Indonesia <teamwars.indo@gmail.com>
2026-09-06 12:36:23 +0700

    Move handleTransferAutocomplete to transfer module

--- lib/discord/handlers/autocomplete-handler.ts
+++ lib/discord/handlers/autocomplete-handler.ts
@@ -2,6 +2,6 @@ export { handleSubmitAutocomplete } from './autocomplete/submit';
 export { handleGameAutocomplete } from './autocomplete/game';
 export {
   handleAssignAutocomplete,
-  handleTransferAutocomplete,
   handleMatchReportAutocomplete,
 } from './autocomplete/admin';
+export { handleTransferAutocomplete } from './autocomplete/transfer';
