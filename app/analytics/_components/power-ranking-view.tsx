a6c746c088006ddbd219800e275028dbd375f25f
Team Wars Indonesia <teamwars.indo@gmail.com>
2026-09-22 04:27:42 +0700

    Add standing prop to PowerRankingView component

--- app/analytics/_components/power-ranking-view.tsx
+++ app/analytics/_components/power-ranking-view.tsx
@@ -281,6 +281,7 @@ export function PowerRankingView({
         isTeamView={isTeamView}
         grandTotal={grandTotal}
         teamLogoMap={teamLogoMap}
+        standing={selectedTeamStanding}
       />
     </div>
   );
